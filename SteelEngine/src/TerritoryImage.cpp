bc2c9a24d1db4c94ccc59fc3696f7c624a2f1683
ludecadon <ludecadon@gmail.com>
2019-07-07 22:13:31 +0800

    Cleaned up TerritoryImage parsing, still needs more processing

--- SteelEngine/src/TerritoryImage.cpp
+++ SteelEngine/src/TerritoryImage.cpp
@@ -10,53 +10,40 @@ namespace Steel
 	{
 		std::vector<Territory*> result;
 
-		BitmapFile *bmp = new BitmapFile(filename);
+		BitmapFile* bmp = new BitmapFile(filename);
 
 		std::vector<BitmapColour> territoryColours;
-		std::vector<std::vector<Vector2d>> territoryBounds;
+		std::vector<Vector2d> territoryBounds;
 		std::vector<Vector2d> castleLocations;
 		std::vector<Vector2d> townLocations;
-		BitmapColour *currentTerritory = nullptr;
-		Vector2d *awaitingCastle = nullptr;
-		Vector2d *awaitingTown = nullptr;
+		BitmapColour* currentTerritory = nullptr;
 
 		int i, j;
-		BitmapColour lastPixel(0,0,0,0);
+		BitmapColour lastPixel(0, 0, 0, 0);
 		//bottom left is 0,0
 		for (i = 0; i < bmp->GetWidth(); i++)
 		{
 			for (j = 0; j < bmp->GetHeight(); j++)
 			{
 				auto pixel = bmp->GetPixelAt(i, j);
-				
+
 				if (pixel.RGBA.R != lastPixel.RGBA.R
 					|| pixel.RGBA.G != lastPixel.RGBA.G
 					|| pixel.RGBA.B != lastPixel.RGBA.B)
 				{
-					printf("Colour = {%d, %d, %d}\n", pixel.RGBA.R, pixel.RGBA.G, pixel.RGBA.B);
+					//change in colour marks a change in territory, hence a territorial boundary should be placed
+					territoryBounds.push_back(Vector2d(i, j));
 				}
 
 				lastPixel = pixel;
 
 				if (pixel.Value == TerritoryImage::CastleColour.Value)
 				{
-					//if the first pixel read is a town or castle, it belongs to the first territory
-					if(awaitingCastle == nullptr)
-						awaitingCastle = new Vector2d(i, j);
-					else
-					{
-						//add it to the current territory
-						castleLocations.push_back(Vector2d(i, j));
-					}
+					castleLocations.push_back(Vector2d(i, j));
 				}
 				else if (pixel.Value == TerritoryImage::TownColour.Value)
 				{
-					if (awaitingTown == nullptr)
-						awaitingTown = new Vector2d(i, j);
-					else
-					{
-						townLocations.push_back(Vector2d(i, j));
-					}
+					townLocations.push_back(Vector2d(i, j));
 				}
 				else
 				{
@@ -73,21 +60,10 @@ namespace Steel
 					if (!found)
 					{
 						territoryColours.push_back(pixel);
-						if (territoryColours.size() == 1)
-						{
-							if (awaitingCastle != nullptr)
-								castleLocations.push_back(Vector2d(awaitingCastle->X, awaitingCastle->Y));
-
-							if (awaitingTown != nullptr)
-								townLocations.push_back(Vector2d(awaitingTown->X, awaitingTown->Y));
-						}
 					}
 				}
 			}
 		}
-
-		delete awaitingCastle;
-		delete awaitingTown;
 
 		return result;
 	}
